28ab797978a0b7da169c190cb51d1c7faa80c586
swatibersurda <swatibersurda@gmail.com>
2022-04-25 16:50:17 +0530

    eval

--- eval25thapril/src/components/Admin.jsx
+++ eval25thapril/src/components/Admin.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 export const Admin = () => {
    const [empcre,setEmpcreate]=useState({
@@ -18,44 +19,69 @@ export const Admin = () => {
 
    })
           const handleValue=(e)=>{
-            console.log(e.target);
+            console.log(e.target.value)
+            console.log(e.target.name);
+            // const {name,value}=e.target;
+
+          // setEmpcreate({...emprc,[name]:value})
+          // setEmpcreate({...empcre,empcre:[name]:value})
+
+          // if(e.target.name==="employee_name")
+          // {
+          //    setEmpcreate({...empcre,employee_name:e.target.value})
+          // }
+
+
+          setEmpcreate({...empcre,[e.target.name]:e.target.value})
+        
+
 
           }
 
+     console.log(empcre);
 
+     const handleStore=(empcre)=>{
+       console.log("hello");
+         
+          axios.post("http://localhost:8080/employee",empcre).then((res)=>{
+           console.log(res.data)
+
+          });
+
+     }
 
 
   return (
-    <form className="createEmployee">
-      <input type="text" placeholder="Employee Name" name="employee_name"  onClick={handleValue}/>
-      <input type="text" placeholder="Employee id" name="employee_id" />
-      <select name="title">
+    <form className="createEmployee" onSubmit={()=>{handleStore(empcre)}}>
+      <input type="text" placeholder="Employee Name" name="employee_name"  onChange={handleValue}/>
+      <input type="text" placeholder="Employee id" name="employee_id" onChange={handleValue} />
+      <select name="title" onChange={handleValue}>
         <option value="intern">Intern</option>
         <option value="Jr Software Developer">Jr Software Developer</option>
         <option value="Sr Software Developer">Sr Software Developer</option>
         <option value="Team Lead">Team Lead</option>
       </select>
-      <input type="number" placeholder="Salary" name="salary" />
-      <input type="text" placeholder="Image" name="image" />
-      <input type="text" placeholder="User Name" name="username" />
-      <input type="password" placeholder="Password" name="password" />
+      <input type="number" placeholder="Salary" name="salary" onChange={handleValue}/>
+      <input type="text" placeholder="Image" name="image" onChange={handleValue} />
+      <input type="text" placeholder="User Name" name="username" onChange={handleValue} />
+      <input type="password" placeholder="Password" name="password" onChange={handleValue} />
       <input
         type="text"
         placeholder="Enter tasks separated by commas"
-        name="tasks"
+        name="tasks" onChange={handleValue}
       />
-      <select name="status" id="status">
+      <select name="status" id="status" onChange={handleValue}>
         <option value="">Select Status</option>
         <option value="terminated">Terminated</option>
         <option value="working">Working</option>
       </select>
-      <select name="team" id="team">
+      <select name="team" id="team" onChange={handleValue}>
         <option value="">Select team</option>
         <option value="frontend">Frontend</option>
         <option value="backend">Backend</option>
         <option value="qa">QA</option>
       </select>
-      <input className="createUser" type="submit" value={"submit"} />
+      <input className="createUser" type="submit" value={"submit"}  />
     </form>
   );
 };
